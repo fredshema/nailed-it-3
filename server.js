04eae400212e79cbd0b57ba5986287bb40c9896e
SHEMA Fred <shemafred3@gmail.com>
2021-07-01 16:06:06 +0200

    fix deploy script

--- server.js
+++ server.js
@@ -18,12 +18,14 @@ app.get("/api/:albumId/images", async (req, res) => {
   data = data.map(formatter);
   res.json(data);
 });
-// set static folder
-app.use(express.static("client/build"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server started on port ${port}`));
