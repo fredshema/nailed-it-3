02956977aedf23ff060cc52fdea2e7b9940ee6e5
SHEMA Fred <shemafred3@gmail.com>
2021-07-01 15:16:49 +0200

    fix build script

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 const express = require("express");
+const path = require("path");
 const fetch = require("node-fetch");
 const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
-
-if (process.env.NODE_ENV === "production") {
-  // set static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 app.get("/api/:albumId/images", async (req, res) => {
   const response = await fetch(
@@ -24,6 +17,11 @@ app.get("/api/:albumId/images", async (req, res) => {
   };
   data = data.map(formatter);
   res.json(data);
+});
+// set static folder
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 const port = 5000;
